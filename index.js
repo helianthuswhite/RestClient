5e1f492fd271ce33191ebcbcfd98589eba8ce5e2
huangyunzhi <huangyunzhi@baidu.com>
2018-11-22 16:06:36 +0800

    npm pub

---
+++ index.js
@@ -0,0 +1,3 @@
+import client from './src/client';
+
+export default client;
--- rollup.config.js
+++ rollup.config.js
@@ -2,15 +2,19 @@
 import resolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
 import uglify from 'rollup-plugin-uglify';
+import {eslint} from 'rollup-plugin-eslint';
 
 export default {
-    input: 'src',
+    input: 'index.js',
     output: {
-        file: 'dist',
+        file: 'restclient.min.js',
         format: 'cjs'
     },
     plugins: [
         resolve(),
+        eslint({
+            exclude: 'node_modules/**'
+        }),
         babel({
             exclude: 'node_modules/**' // 只编译我们的源代码
         }),

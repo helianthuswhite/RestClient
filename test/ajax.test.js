9f1d0ac43403842b6731abf4327d29250ab7e124
huangyunzhi <huangyunzhi@baidu.com>
2019-02-14 16:58:18 +0800

    testEnvironment jsdom

--- test/ajax.test.js
+++ test/ajax.test.js
@@ -2,11 +2,16 @@ import Ajax from '../src/ajax';
 
 describe('Testing Ajax Class', () => {
 
+    const ajax = new Ajax({});
+
     test('Ajax instance should have config property and tow Plugins', () => {
-        // const ajax = new Ajax({});
-        // expect(ajax.config).toEqual({});
-        // expect(typeof ajax.req).toBe('Plugin');
-        // expect(typeof ajax.res).toBe('Plugin');
+        expect(ajax.hasOwnProperty('config')).toBe(true);
+        expect(typeof ajax.req).toBe('object');
+        expect(typeof ajax.res).toBe('object');
+    });
+
+    test('new Ajax().request should return a promise', () => {
+        expect(ajax.request()  instanceof Promise).toBe(true);
     });
 
 });

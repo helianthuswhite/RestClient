4e9168aff875c395a22fc51a572c9ac23a519493
helianthuswhite <hyz19960229@gmail.com>
2022-02-16 15:26:46 +0800

    fix types

--- types/client.d.ts
+++ types/client.d.ts
@@ -6,13 +6,13 @@
  */
 import { TSObject, Ajax, Options } from './ajax';
 export class Client extends Ajax {
-    __bulkRequest(url: string, method: string, config?: Options, querys?: TSObject, data?: TSObject): Promise<unknown>;
-    head(url: string, config?: Options): Promise<unknown>;
-    options(url: string, config?: Options): Promise<unknown>;
-    patch(url: string, data?: TSObject, config?: Options): Promise<unknown>;
-    get(url: string, querys?: TSObject, config?: Options): Promise<unknown>;
-    delete(url: string, querys?: TSObject, config?: Options): Promise<unknown>;
-    post(url: string, data?: TSObject, config?: Options): Promise<unknown>;
-    put(url: string, data?: TSObject, config?: Options): Promise<unknown>;
+    __bulkRequest(url: string, method: string, config?: Options | TSObject, querys?: TSObject, data?: TSObject): Promise<unknown>;
+    head(url: string, config?: Options | TSObject): Promise<unknown>;
+    options(url: string, config?: Options | TSObject): Promise<unknown>;
+    patch(url: string, data?: TSObject, config?: Options | TSObject): Promise<unknown>;
+    get(url: string, querys?: TSObject, config?: Options | TSObject): Promise<unknown>;
+    delete(url: string, querys?: TSObject, config?: Options | TSObject): Promise<unknown>;
+    post(url: string, data?: TSObject, config?: Options | TSObject): Promise<unknown>;
+    put(url: string, data?: TSObject, config?: Options | TSObject): Promise<unknown>;
     __decoratorMiddleware(fn: Function, key: string, ...args: Array<any>[]): any;
 }
